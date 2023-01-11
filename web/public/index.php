15ac2eaf864b5f04e045d374debfbe0b2d4ecebf
Marius Lenzing <marius.lenzing@twt.de>
2023-01-11 21:01:24 +0100

    added improvisiertes favicon

--- web/public/index.php
+++ web/public/index.php
@@ -138,12 +138,18 @@
     }
     catch (Throwable $e) {}
 ?>
+
 <!DOCTYPE html>
 <html lang="de">
     <head>
         <meta charset="utf-8">
         <title>Rhizom</title>
         <link rel="stylesheet" href="assets/css/style.css">
+        <!-- favicon -->
+        <link rel="apple-touch-icon" sizes="180x180" href="assets/img/favicon/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicon/favicon-16x16.png">
+        <link rel="manifest" href="assets/img/favicon/site.webmanifest">
     </head>
     <body>
         <header>
